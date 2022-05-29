08fc47d88975b8c13c538b6561721b62c8a68cd9
mel <mel@melmassadian.com>
2022-05-30 01:30:29 +0200

    fix: ⚡️ imports, remove license per file

--- frame_jump.py
+++ frame_jump.py
@@ -1,47 +1,9 @@
-# BSD 3-Clause License
-#
-# Copyright (c) 2020, Boris FX
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1. Redistributions of source code must retain the above copyright notice, this
-#    list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright notice,
-#    this list of conditions and the following disclaimer in the documentation
-#    and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the copyright holder nor the names of its
-#    contributors may be used to endorse or promote products derived from
-#    this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-import sys
-from mocha_common_tools import *
-from mocha.tools import *
-
-# If v6, use Pyside2. If V5 or earlier use Pyside
-try:
-    from PySide import QtGui, QtCore
-    from PySide.QtGui import *
-    from PySide.QtCore import *
-except ImportError:
-    from PySide2 import QtCore, QtWidgets
-    # from PySide2.QtGui import *
-    from PySide2.QtCore import *
-    from PySide2.QtWidgets import *
+from mocha_common_tools import (
+    set_current_playhead_time,
+    get_current_playhead_time,
+)
+from PySide2.QtCore import Qt, QCoreApplication
+from PySide2.QtWidgets import QDialog, QLineEdit, QPushButton, QHBoxLayout
 
 from mocha.project import get_current_project
 from mocha.ui import get_widgets
